e075cd48e92e405ba0a234f8b0647811cf16c7cb
Mubarak Mikail <mubarak@Mubaraks-MacBook-Pro.local>
2020-09-01 18:52:12 +0200

    using supertest package

---
+++ tests/blog_api.test.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('Blogs are returned as json', () => {
+    api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
