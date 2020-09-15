34cf8bbd9124f9fd94c43831185dc684ac896b4f
Mubarak Mikail <mikail.mubarak1@gmail.com>
2020-09-15 12:59:00 +0200

    4.18: bloglist expansion, step6

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
@@ -21,5 +22,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
